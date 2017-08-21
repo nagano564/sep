74661b9963312575d9fa003728c09f2cdf7a14a2
nagano564 <nagano564@gmail.com>
2017-08-21 13:21:18 -0400

    stack and queues done

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,17 @@ class MyQueue
   end
 
   def enqueue(element)
+    enqueue = @queue.insert(0, element)
+    @head = @queue[-1]
+    @tail = @queue[0]
   end
 
   def dequeue
+    dequeue = @queue.delete_at(-1)
+    @tail = @queue[0]
   end
 
   def empty?
+    empty = @queue.count == 0
   end
 end
