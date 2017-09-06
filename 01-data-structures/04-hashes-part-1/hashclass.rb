4da203a30f7de78fd5cde8fe9e0f7fab66197f82
nagano564 <nagano564@gmail.com>
2017-09-06 18:23:57 -0400

    this doesn't belong here

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -6,18 +6,24 @@ class HashClass
 
   def []=(key, value)
   end
-
+  # my_hash = HashClass.new(5)
+  # my_hash["key"] = "value"
 
   def [](key)
   end
+  # value = my_hash["key"]
 
   def resize
+    #if index == index mulitiply index * 2
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    ascii = key.sum
+
+    ascii
   end
 
   # Simple method to return the number of items in the hash
