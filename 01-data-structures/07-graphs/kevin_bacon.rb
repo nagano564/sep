3a316b79aded7651dae7e9350996809198d1125d
nagano564 <nagano564@gmail.com>
2017-11-15 15:50:38 -0500

    working on kevin

--- 01-data-structures/07-graphs/kevin_bacon.rb
+++ 01-data-structures/07-graphs/kevin_bacon.rb
@@ -4,7 +4,26 @@ class Bacon
   end
 
 
-  def find_kevin_bacon
+  def find_kevin_bacon(actor)
+    # return array of movies to connect kevin bacon
+    target_actor = "Kevin Bacon"
+    answer_array =[]
+    # fails if over 6
+    if answer_array.length >= 6 return nil
+      # push keys into answer_array
+      current_actor = actor
 
+      # Movies.film_actor_hash.each do |key, value|
+      #   #  if current_actor & actor in the same value_array
+      #       # push key into answer_array
+      #     elsif find another key with current_actor as value
+      #       loop thru value_array to find kevin_bacon
+      #       no kevin_bacon so find movie with array[all] starting at 0
+      #       set searching key to new movies
+      #       loop thru value_array to find kevin_bacon
+      #       no kevin_bacon so find movie with array[all] starting at 0
+      #       set searching key to new movies
+      #     else no kevin bacon link
+      end
   end
 end
