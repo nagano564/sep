b661b30f871c6a318bbcd81ac5736529562d615b
nagano564 <nagano564@gmail.com>
2017-11-19 11:27:08 -0500

    stuck

--- 01-data-structures/07-graphs/kevin_bacon.rb
+++ 01-data-structures/07-graphs/kevin_bacon.rb
@@ -3,19 +3,32 @@ class Bacon
     "success"
   end
 
+  def find(actor_name)
+    answer = Array.new
+    current_actor = actor_name
+    hash.each do |movie, actors|
+      if actors.include? current_actor
+        answer << movie
+  	else
+  		current_actor = ????
+      end
+    end
+    answer
+  end
 
   def find_kevin_bacon(actor)
     # return array of movies to connect kevin bacon
     target_actor = "Kevin Bacon"
-    answer_array =[]
-    # fails if over 6
-    if answer_array.length >= 6 return nil
-      # push keys into answer_array
-      current_actor = actor
+    answer =[]
+    current_actor = actor
 
-    Movies.film_actor_hash.each do |key, value|
+    # fails if over 6
+    if answer.length >= 6 return nil
+      # push keys into answer_array
+
+    Movies.film_actor_hash.each do |movie, actor|
      if current_actor == actor in the same value_array
-        answer_array.push(key)
+
       elsif find another key with current_actor as value
         loop thru value_array to find kevin_bacon
         no kevin_bacon so find movie with array[all] starting at 0
@@ -28,3 +41,9 @@ class Bacon
     end
   end
 end
+# Here is what you would do if you wanted to write using BFS. 1. Make the movies into a graph.
+# 2. Have a queue of `nodes_to_visit` and a set `visited`. 3. Set `current_actor= "kevin bacon"`.
+# 4. Push every direct connection of `current_actor` into `nodes_to_visit` and add `current_actor`
+# to `visited`.  5. If `nodes_to_visit` is empty or `visited` size equals the number of actors,
+# it cannot be found. 6. Pop `nodes_to_visit` and set `current_actor` to this value.
+# 7. If `current_actor` exists in `visited` then skip and repeat previous step. 8. Otherwise, repeat step 4.
