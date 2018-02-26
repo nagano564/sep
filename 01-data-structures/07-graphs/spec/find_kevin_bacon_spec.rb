d05afabcd92f3637275c0912bad79497edcb0880
nagano564 <nagano564@gmail.com>
2018-02-26 15:01:30 -0500

    working on bacon

--- 01-data-structures/07-graphs/spec/find_kevin_bacon_spec.rb
+++ 01-data-structures/07-graphs/spec/find_kevin_bacon_spec.rb
@@ -1,5 +1,54 @@
-RSpec.describe FindKevinBacon do
-  it "should test methods in FindKevinBacon.rb" do
-    expect(FindKevinBacon.some_method).to eq "success"
+require '../find_kevin_bacon'
+
+RSpec.describe FindKevinBacon, type: Class do
+
+  let (:kevin_bacon) { Node.new("Kevin Bacon") }
+  let (:lori_singer) { Node.new("Lori Singer") }
+  let (:john_lithgow) { Node.new("John Lithgow") }
+  let (:chris_penn) { Node.new("Chris Penn") }
+  let (:matthew_mcconaughey) { Node.new("Matthew McConaughey") }
+  let (:anne_hathaway) { Node.new("Anne Hathaway") }
+  let (:zoe_saldanas) { Node.new("Zoe Saldanas") }
+  let (:bill_paxton) { Node.new("Bill Paxton") }
+  let (:tom_hanks) { Node.new("Tom Hanks") }
+  let (:jeff_bridges) { Node.new("Jeff Bridges") }
+  let (:julianne_moore) { Node.new("Julianne Moore") }
+  let (:vin_diesel) { Node.new("Vin Diesel") }
+  let (:matt_damon) { Node.new("Matt Damon") }
+  let (:paul_giamatti) { Node.new("Paul Giamatti") }
+  let (:ted_danson) { Node.new("Ted Danson") }
+  let (:tim_roth) { Node.new("Tim Roth") }
+  let (:al_pacino) { Node.new("Al Pacino") }
+  let (:johnny_depp) { Node.new("Johnny Depp") }
+  let (:joe_cross) { Node.new("Joe Cross") }
+  let (:amy_badberg) { Node.new("Amy Badberg") }
+  let (:merv_cross) { Node.new("Merv Cross") }
+  let (:alec_baldwin) { Node.new("Alec Baldwin") }
+  let (:kevin_spacey) { Node.new("Kevin Spacey") }
+
+  let (:graph) {FindKevinBacon.new(kevin_bacon)}
+
+  before (:each) do
+    kevin_bacon.film_actor_hash["FootLoose"] = [lori_singer, john_lithgow, chris_penn]
+    bill_paxton.film_actor_hash["Appolo 13"] = [kevin_bacon, tom_hanks]
+    tom_hanks.film_actor_hash["Saving Private Ryan"] = [paul_giamatti, ted_danson, matt_damon]
+    john_lithgow.film_actor_hash["Interstellar"] = [matthew_mcconaughey, anne_hathaway, lori_singer]
+    paul_giamatti.film_actor_hash["Donnie Brasco"] = [al_pacino, johnny_depp]
+    joe_cross.film_actor_hash["Fat, Sick and nearly dead"] = [amy_badberg, merv_cross]
+    al_pacino.film_actor_hash["Glengarry Glen Ross"] = [alec_baldwin, kevin_spacey]
+  end
+
+  describe "testing the node" do
+    it "test node to read property" do
+      expect(kevin_bacon.name).to eq("Kevin Bacon")
+    end
+
+    it "finds kevin back if he is in a movie - one degree" do
+      expect{(graph.find_kevin_bacon(bill_paxton))}.to output("[\"Appolo 13\"]\n").to_stdout
+    end
+
+    it "finds kevin if he is associated with an actor in another film matt_damon => tom_hanks => kevin_bacon" do
+      expect{(graph.find_kevin_bacon(paul_giamatti))}.to output("[\"Saving Private Ryan\", \"Appolo13\"]\n").to_stdout
+    end
   end
 end
