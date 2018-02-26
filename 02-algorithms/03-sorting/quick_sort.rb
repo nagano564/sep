ff35215f3880ee3cf5af7ae5778afbdad6ad470b
nagano564 <nagano564@gmail.com>
2018-02-26 15:25:58 -0500

    refactor

--- 02-algorithms/03-sorting/quick_sort.rb
+++ 02-algorithms/03-sorting/quick_sort.rb
@@ -6,9 +6,11 @@ def quick_sort(array, low, high)
     array
   else
     if low < high
-      x = partition(array, low, high)
-      quick_sort(array, low, x - 1 )
-      quick_sort(array, x + 1, high)
+      pivot = partition(array, low, high)
+      # low half
+      quick_sort(array, low, pivot - 1 )
+      # high
+      quick_sort(array, pivot + 1, high)
     end
   end
   return array
@@ -18,21 +20,23 @@ def partition(array, low, high)
   pivot = array[high]
   index = low
 
-  for i in (low...high)
+  # for i in (low...high)
+  (low...high).each do |i|
     if array[i] <= pivot
-      # swap i and index
       temp = array[i]
       array[i] = array[index]
       array[index] = temp
       index += 1
     end
   end
+  # swapping array[index] with array[high] to set a new pivot point
+  #
   temp = array[index]
   array[index] = array[high]
   array[high] = temp
   return index
 end
 
-array = [248, 185, 22, 288, 128, 234, 24, 206, 220, 25, 101, 91, 107, 50, 130, 229, 124, 249, 274, 92, 155, 186, 236, 32, 88, 86, 84, 273, 183, 213, 11, 232, 190, 108, 176, 167, 35, 241, 71, 16, 228, 194, 165, 179, 56, 264, 154, 191, 272, 64]
-
-puts Benchmark.measure {quick_sort(array, 0, -1)}
+array = [248, 185, 22, 288, 128, 234, 24, 206, 220, 25, 101, 91]
+puts quick_sort(array, 0 , array.length - 1)
+puts Benchmark.measure {quick_sort(array, 0, array.length - 1)}
