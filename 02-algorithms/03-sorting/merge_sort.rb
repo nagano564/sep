ba941a3744631d9aecb5aeb342ae9fb23e89775b
nagano564 <nagano564@gmail.com>
2018-03-03 21:02:55 -0500

    refactor

--- 02-algorithms/03-sorting/merge_sort.rb
+++ 02-algorithms/03-sorting/merge_sort.rb
@@ -1,17 +1,17 @@
 require 'benchmark'
+require 'byebug'
 
 def merge_sort(collection)
   # #1
-  if collection.length <= 1
-    collection
-  else
+  return collection if collection.length <= 1
     # #2
     mid = (collection.length / 2).floor
     left = merge_sort(collection[0..mid - 1])
     right = merge_sort(collection[mid..collection.length])
     # #3
     merge(left, right)
-  end
+    # byebug
+
 end
 
 def merge(left, right)
@@ -30,6 +30,5 @@ def merge(left, right)
   end
 end
 
-array = [248, 185, 22, 288, 128, 234, 24, 206, 220, 25, 101, 91, 107, 50, 130, 229, 124, 249, 274, 92, 155, 186, 236, 32, 88, 86, 84, 273, 183, 213, 11, 232, 190, 108, 176, 167, 35, 241, 71, 16, 228, 194, 165, 179, 56, 264, 154, 191, 272, 64]
-
-puts Benchmark.measure {merge_sort(array)}
+array = [248, 185, 22, 288, 128, 234, 24, 206, 220, 25, 101, 91, 107]
+puts merge_sort(array).inspect
