248dee43c2cbf6f3ed6f202fd63304f0b03f85bb
nagano564 <nagano564@gmail.com>
2018-03-10 20:01:00 -0500

    set back

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -29,12 +29,22 @@ class BinarySearchTree
   end
 
   # Recursive Depth First Search
-  def find(root, search)
-    return if root.nil? || search.nil?
+  def find(root, data)
+	  if data.nil?
+		  return nil
+	  elsif root.title == data
+		  return root
+	  end
 
-    return root if root.title == search
+	  unless root.left.nil?
+      answer = find(root.left, data)
+      return answer unless answer == nil
+	  end
 
-    find(root.left, search) || find(root.right, search)
+	  unless root.right.nil?
+		  return find(root.right, data)
+	  end
+    return nil
   end
 
   def delete(root, data)
