cdc8aed4404377f1ce9935cf998b6739636c2bf0
nagano564 <nagano564@gmail.com>
2018-03-12 17:56:46 -0400

    refactor

--- 02-algorithms/03-sorting/selection_sort.rb
+++ 02-algorithms/03-sorting/selection_sort.rb
@@ -11,26 +11,19 @@ require 'benchmark'
 
 def selection_sort(collection)
   length = collection.length
-  # #1
-  for i in 0..length - 2
-    # #2
+
+   (0..length - 2).each do |i|
     min_index = i
-    # #3
-    for j in (i + 1)...length
+
+    ((i + 1)...length).each do |j|
       if collection[j] < collection[min_index]
-        # #4
         min_index = j
       end
     end
-
-    # #5
-    tmp = collection[i]
-    collection[i] = collection[min_index]
-    collection[min_index] = tmp
+    collection[i], collection[min_index] = collection[min_index], collection[i]
   end
   collection
 end
 
-array = [248, 185, 22, 288, 128, 234, 24, 206, 220, 25, 101, 91, 107, 50, 130, 229, 124, 249, 274, 92, 155, 186, 236, 32, 88, 86, 84, 273, 183, 213, 11, 232, 190, 108, 176, 167, 35, 241, 71, 16, 228, 194, 165, 179, 56, 264, 154, 191, 272, 64]
-
-puts Benchmark.measure {selection_sort(array)}
+array = [248, 185, 22, 288, 128, 234, 24, 206, 220]
+puts selection_sort(array).inspect
