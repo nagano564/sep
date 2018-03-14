1562cd28c178b84690d39cb2857408ec6772695e
nagano564 <nagano564@gmail.com>
2018-03-13 23:22:29 -0400

    fixed binary recursive

--- 02-algorithms/02-searching/recursive_binary_search.rb
+++ 02-algorithms/02-searching/recursive_binary_search.rb
@@ -2,8 +2,10 @@ def recursive_binary_search (collection, value)
   min = 0
   max = collection.length - 1
   middle = ((min + max)/2)
-  if collection[middle] == value
-    middle
+  if collection.length == 0
+    puts "not found"
+  elsif collection[middle] == value
+    puts "found value"
   elsif collection[middle] < value
     recursive_binary_search(collection[middle+1..max],value)
   else
@@ -12,4 +14,5 @@ def recursive_binary_search (collection, value)
 end
 
 
-collection = [4,2,5,7,1]
+collection = [1,3,5,7,9]
+recursive_binary_search(collection, 12)
